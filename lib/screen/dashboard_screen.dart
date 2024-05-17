dac7f29f85ee5672229550fc89e69bf873dfa8e6
BibekKumarKushwaha <bibekkumar345@gmail.com>
2024-05-17 23:47:21 +0545

    dashboard

--- lib/screen/dashboard_screen.dart
+++ lib/screen/dashboard_screen.dart
@@ -7,7 +7,7 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('dashboard'),
+        child: Text('dashboard screen'),
       ),
     );
   }
