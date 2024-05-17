e528ab7a48d53ac8b045d65e1736b51484ceb081
BibekKumarKushwaha <bibekkumar345@gmail.com>
2024-05-17 20:15:20 +0545

    minor changes

--- lib/screen/signup_screen.dart
+++ lib/screen/signup_screen.dart
@@ -35,169 +35,172 @@ class SignupScreen extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.fromLTRB(16.0, 260.0, 16.0, 0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Text(
-                    'Sign Up',
-                    style: TextStyle(
-                      fontSize: 32.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue[800],
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 24.0),
-                  _buildTextField(
-                    controller: _firstNameController,
-                    label: 'First Name',
-                    icon: Icons.person,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your first name';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16.0),
-                  _buildTextField(
-                    controller: _lastNameController,
-                    label: 'Last Name',
-                    icon: Icons.person,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your last name';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16.0),
-                  _buildTextField(
-                    controller: _emailController,
-                    label: 'Email',
-                    icon: Icons.email,
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter an email';
-                      }
-                      if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                        return 'Please enter a valid email';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16.0),
-                  _buildPasswordField(
-                    controller: _passwordController,
-                    label: 'Password',
-                    icon: Icons.lock,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a password';
-                      }
-                      if (value.length < 6) {
-                        return 'Password must be at least 6 characters';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16.0),
-                  _buildPasswordField(
-                    controller: _confirmPasswordController,
-                    label: 'Confirm Password',
-                    icon: Icons.lock,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please confirm your password';
-                      }
-                      if (value != _passwordController.text) {
-                        return 'Passwords do not match';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 24.0),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.white,
-                      backgroundColor: Colors.blue[800],
-                      padding: const EdgeInsets.symmetric(vertical: 16.0),
-                      textStyle: const TextStyle(fontSize: 16.0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        fontSize: 32.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue[800],
                       ),
+                      textAlign: TextAlign.center,
                     ),
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        // Perform signup action
-                      }
-                    },
-                    child: const Text('Sign Up'),
-                  ),
-                  const SizedBox(height: 24.0),
-                  Text(
-                    'Or sign up with',
-                    style: TextStyle(
-                      color: Colors.blue[800],
-                      fontSize: 16.0,
+                    const SizedBox(height: 24.0),
+                    _buildTextField(
+                      controller: _firstNameController,
+                      label: 'First Name',
+                      icon: Icons.person,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your first name';
+                        }
+                        return null;
+                      },
                     ),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 16.0),
-                  _buildSocialLoginButton(
-                    'Sign Up with Facebook',
-                    'assets/images/fb_logo.png',
-                    Colors.blue[800]!,
-                    () {
-                      // Perform Facebook signup action
-                    },
-                  ),
-                  const SizedBox(height: 16.0),
-                  _buildSocialLoginButton(
-                    'Sign Up with Google',
-                    'assets/images/google_logo.png',
-                    Colors.red,
-                    () {
-                      // Perform Google signup action
-                    },
-                  ),
-                  const SizedBox(height: 16.0),
-                  _buildSocialLoginButton(
-                    'Sign Up with Apple',
-                    'assets/images/mac_logo.png',
-                    Colors.black,
-                    () {
-                      // Perform Apple signup action
-                    },
-                  ),
-                  const SizedBox(height: 24.0),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: RichText(
-                      text: const TextSpan(
-                        text: "Already have an account? ",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16.0,
+                    const SizedBox(height: 16.0),
+                    _buildTextField(
+                      controller: _lastNameController,
+                      label: 'Last Name',
+                      icon: Icons.person,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your last name';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16.0),
+                    _buildTextField(
+                      controller: _emailController,
+                      label: 'Email',
+                      icon: Icons.email,
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter an email';
+                        }
+                        if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                          return 'Please enter a valid email';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16.0),
+                    _buildPasswordField(
+                      controller: _passwordController,
+                      label: 'Password',
+                      icon: Icons.lock,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a password';
+                        }
+                        if (value.length < 6) {
+                          return 'Password must be at least 6 characters';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16.0),
+                    _buildPasswordField(
+                      controller: _confirmPasswordController,
+                      label: 'Confirm Password',
+                      icon: Icons.lock,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please confirm your password';
+                        }
+                        if (value != _passwordController.text) {
+                          return 'Passwords do not match';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 24.0),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.blue[800],
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        textStyle: const TextStyle(fontSize: 16.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
                         ),
-                        children: [
-                          TextSpan(
-                            text: 'Login',
-                            style: TextStyle(
-                              color: Colors.blue,
-                              fontSize: 16.0,
-                              decoration: TextDecoration.underline,
-                            ),
+                      ),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          // Perform signup action
+                        }
+                      },
+                      child: const Text('Sign Up'),
+                    ),
+                    const SizedBox(height: 24.0),
+                    Text(
+                      'Or sign up with',
+                      style: TextStyle(
+                        color: Colors.blue[800],
+                        fontSize: 16.0,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 16.0),
+                    _buildSocialLoginButton(
+                      'Sign Up with Facebook',
+                      'assets/images/fb_logo.png',
+                      Colors.blue[800]!,
+                      () {
+                        // Perform Facebook signup action
+                      },
+                    ),
+                    const SizedBox(height: 16.0),
+                    _buildSocialLoginButton(
+                      'Sign Up with Google',
+                      'assets/images/google_logo.png',
+                      Colors.red,
+                      () {
+                        // Perform Google signup action
+                      },
+                    ),
+                    const SizedBox(height: 16.0),
+                    _buildSocialLoginButton(
+                      'Sign Up with Apple',
+                      'assets/images/mac_logo.png',
+                      Colors.black,
+                      () {
+                        // Perform Apple signup action
+                      },
+                    ),
+                    const SizedBox(height: 24.0),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: RichText(
+                        text: const TextSpan(
+                          text: "Already have an account? ",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16.0,
                           ),
-                        ],
+                          children: [
+                            TextSpan(
+                              text: 'Login',
+                              style: TextStyle(
+                                color: Colors.blue,
+                                fontSize: 16.0,
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ],
