d80884bf489293186679177ed4654131e5cb6309
BibekKumarKushwaha <bibekkumar345@gmail.com>
2024-05-17 23:45:52 +0545

    log in

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -41,7 +41,7 @@ class LoginScreen extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         Text(
-                          'Log In Pannel',
+                          'Log In',
                           style: TextStyle(
                             fontSize: 32.0,
                             fontWeight: FontWeight.bold,
