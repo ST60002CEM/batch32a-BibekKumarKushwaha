c3c04a3ce6822d449e45b47fa1e15cbc4e6f75b4
BibekKumarKushwaha <bibekkumar345@gmail.com>
2024-05-17 20:18:05 +0545

    splash

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -4,6 +4,8 @@ import 'package:final_assignment/screen/login_screen.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
