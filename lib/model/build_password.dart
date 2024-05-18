ec23738bd034cc1564ffaac1df9f7384ec5bdc0a
BibekKumarKushwaha <bibekkumar345@gmail.com>
2024-05-18 12:24:33 +0545

    dialog box

---
+++ lib/model/build_password.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+Widget _buildPasswordField({
+    required TextEditingController controller,
+    required String label,
+    required IconData icon,
+    bool obscureText = true,
+    TextInputType keyboardType = TextInputType.text,
+    String? Function(String?)? validator,
+  }) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: label,
+        prefixIcon: Icon(icon, color: Colors.blue[800]),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+      ),
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      validator: validator,
+    );
+  }
