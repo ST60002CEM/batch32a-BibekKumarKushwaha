77fbde4f5ff35d3360a0e7ecf5ba7171e4d19156
BibekKumarKushwaha <bibekkumar345@gmail.com>
2024-06-21 14:34:14 +0545

    api end point

--- lib/app/constants/api_endpoint.dart
+++ lib/app/constants/api_endpoint.dart
@@ -3,19 +3,11 @@ class ApiEndpoints {
 
   static const Duration connectionTimeout = Duration(seconds: 1000);
   static const Duration receiveTimeout = Duration(seconds: 1000);
-  static const String baseUrl = "http://10.0.2.2:3000/api/v1/";
+  static const String baseUrl = "http://10.0.2.2:5000/api/";
   //static const String baseUrl = "http://localhost:3000/api/v1/";
   //static const String baseUrl = "http://192.168.4.4:3000/api/v1/";
 
   // ====================== Auth Routes ======================
-  static const String login = "auth/login";
-  static const String register = "auth/register";
-  
-  
-  static const String updateStudent = "auth/updateStudent/";
-  static const String deleteStudent = "auth/deleteStudent/";
-  static const String imageUrl = "http://10.0.2.2:3000/uploads/";
-  static const String uploadImage = "auth/uploadImage";
-  static const String currentUser = "auth/currentUser";
-
+  static const String login = "user/login";
+  static const String register = "user/create";
 }
