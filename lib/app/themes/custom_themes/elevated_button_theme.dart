d2992fb638b4d0cda6838260c50337265d15b49f
BibekKumarKushwaha <bibekkumar345@gmail.com>
2024-08-08 16:38:54 +0545

    button theme

---
+++ lib/app/themes/custom_themes/elevated_button_theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class KElevatedButtonTheme {
+  KElevatedButtonTheme._();
+
+  static final lightElevatedButtonTheme = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.blue,
+      disabledBackgroundColor: Colors.grey,
+      disabledForegroundColor: Colors.grey,
+      side: const BorderSide(color: Colors.blue),
+      textStyle: const TextStyle(
+          fontSize: 16, color: Colors.white, fontWeight: FontWeight.w600),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    ),
+  );
+
+  static final darkElevatedButtonTheme = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.black,
+      disabledBackgroundColor: Colors.grey,
+      disabledForegroundColor: Colors.grey,
+      side: const BorderSide(color: Colors.blue),
+      textStyle: const TextStyle(
+          fontSize: 16, color: Colors.white, fontWeight: FontWeight.w600),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    ),
+  );
+}
