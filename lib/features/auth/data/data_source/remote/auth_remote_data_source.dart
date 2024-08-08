c1656a1c64cc5b568a4caaf19e3ec94d1ceb6c6b
BibekKumarKushwaha <bibekkumar345@gmail.com>
2024-08-08 16:39:29 +0545

    auth remote data source

--- lib/features/auth/data/data_source/remote/auth_remote_data_source.dart
+++ lib/features/auth/data/data_source/remote/auth_remote_data_source.dart
@@ -45,6 +45,7 @@ class AuthRemoteDatasource {
       return Left(Failure(error: e.toString()));
     }
   }
+  
 
   Future<Either<Failure, bool>> loginUser(
       {required String email, required String password}) async {
@@ -68,4 +69,82 @@ class AuthRemoteDatasource {
       return Left(Failure(error: e.error.toString()));
     }
   }
+  Future<Either<Failure, AuthEntity>> getMe() async {
+    try {
+      String? token;
+      final data = await userSharedprefs.getUserToken();
+      data.fold(
+        (l) => token = null,
+        (r) => token = r,
+      );
+      Response response = await dio.get(
+        ApiEndpoints.getMe,
+        options: Options(
+          headers: {'Authorization': 'Bearer $token'},
+        ),
+      );
+ 
+      if (response.statusCode == 200) {
+        return Right(AuthApiModel.fromJson(response.data['user']).toEntity());
+      }
+      return Left(
+        Failure(
+            error: response.data['message'],
+            statusCode: response.statusCode.toString()),
+      );
+    } on DioException catch (e) {
+      return Left(Failure(error: e.error.toString()));
+    }
+  }
+
+  
+
+  Future<Either<Failure, bool>> fingerPrintLogin(String id) async {
+    try {
+      Response response = await dio.post(
+        ApiEndpoints.getToken,
+        data: {'id': id},
+      );
+      if (response.statusCode == 200) {
+        final token = response.data['token'];
+        await userSharedprefs.setUserToken(token);
+        return const Right(true);
+      }
+      return Left(
+        Failure(
+            error: response.data['message'],
+            statusCode: response.statusCode.toString()),
+      );
+    } on DioException catch (e) {
+      return Left(Failure(error: e.error.toString()));
+    }
+  }
+ 
+  Future<Either<Failure, bool>> verifyUser() async {
+    try {
+      String? token;
+      final data = await userSharedprefs.getUserToken();
+      data.fold(
+        (l) => token = null,
+        (r) => token = r,
+      );
+      Response response = await dio.get(
+        ApiEndpoints.verifyUser,
+        options: Options(
+          headers: {'Authorization': 'Bearer $token'},
+        ),
+      );
+      if (response.statusCode == 200) {
+        return const Right(true);
+      }
+      return Left(
+        Failure(
+            error: response.data['message'],
+            statusCode: response.statusCode.toString()),
+      );
+    } on DioException catch (e) {
+      return Left(Failure(error: e.error.toString()));
+    }
+  }
 }
+
