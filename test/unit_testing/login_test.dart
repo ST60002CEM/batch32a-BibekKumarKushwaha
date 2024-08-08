21396989bedffd7f4477798e15d6154d60cd515c
BibekKumarKushwaha <bibekkumar345@gmail.com>
2024-08-08 16:43:37 +0545

    test

--- test/unit_testing/login_test.dart
+++ test/unit_testing/login_test.dart
@@ -50,113 +50,113 @@
   
 //   registerUser(testUser) {}
 // }
-import 'package:dartz/dartz.dart';
-import 'package:final_assignment/core/failure/failure.dart';
-import 'package:final_assignment/features/auth/domain/entity/auth_entity.dart';
-import 'package:final_assignment/features/auth/domain/usecases/auth_usecase.dart';
-import 'package:final_assignment/features/auth/presentation/navigator/login_navigator.dart';
-import 'package:final_assignment/features/auth/presentation/navigator/register_navigator.dart';
-import 'package:final_assignment/features/auth/presentation/viewmodel/auth_view_model.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:final_assignment/core/failure/failure.dart';
+// import 'package:final_assignment/features/auth/domain/entity/auth_entity.dart';
+// import 'package:final_assignment/features/auth/domain/usecases/auth_usecase.dart';
+// import 'package:final_assignment/features/auth/presentation/navigator/login_navigator.dart';
+// import 'package:final_assignment/features/auth/presentation/navigator/register_navigator.dart';
+// import 'package:final_assignment/features/auth/presentation/viewmodel/auth_view_model.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:mockito/annotations.dart';
+// import 'package:mockito/mockito.dart';
  
-import 'auth_test.mocks.dart';
+// import 'auth_test.mocks.dart';
  
-@GenerateNiceMocks([
-  MockSpec<AuthUseCase>(),
-  MockSpec<LoginViewNavigator>(),
-  MockSpec<RegisterViewNavigator>(),
-])
-void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
+// @GenerateNiceMocks([
+//   MockSpec<AuthUseCase>(),
+//   MockSpec<LoginViewNavigator>(),
+//   MockSpec<RegisterViewNavigator>(),
+// ])
+// void main() {
+//   TestWidgetsFlutterBinding.ensureInitialized();
  
-  late AuthUseCase mockAuthUsecase;
-  late ProviderContainer container;
-  late LoginViewNavigator mockLoginViewNavigator;
+//   late AuthUseCase mockAuthUsecase;
+//   late ProviderContainer container;
+//   late LoginViewNavigator mockLoginViewNavigator;
  
-  setUp(() {
-    mockAuthUsecase = MockAuthUseCase();
-    mockLoginViewNavigator = MockLoginViewNavigator();
-    container = ProviderContainer(
-      overrides: [
-        authViewModelProvider.overrideWith(
-          (ref) => AuthViewModel(mockLoginViewNavigator, mockAuthUsecase),
-        ),
-      ],
-    );
-  });
-  tearDown(
-    () {
-      container.dispose();
-    },
-  );
+//   setUp(() {
+//     mockAuthUsecase = MockAuthUseCase();
+//     mockLoginViewNavigator = MockLoginViewNavigator();
+//     container = ProviderContainer(
+//       overrides: [
+//         authViewModelProvider.overrideWith(
+//           (ref) => AuthViewModel(mockLoginViewNavigator, mockAuthUsecase),
+//         ),
+//       ],
+//     );
+//   });
+//   tearDown(
+//     () {
+//       container.dispose();
+//     },
+//   );
  
-  test("Checking Initial State", () {
-    final authState = container.read(authViewModelProvider);
-    expect(authState.isLoading, false);
-    expect(authState.error, isNull);
-  });
-  test("Login with valid credentials", () async {
-    //Arrange
-    const correctEmail = 'admin@gmail.com';
-    const correctPassword = 'admin123';
+//   test("Checking Initial State", () {
+//     final authState = container.read(authViewModelProvider);
+//     expect(authState.isLoading, false);
+//     expect(authState.error, isNull);
+//   });
+//   test("Login with valid credentials", () async {
+//     //Arrange
+//     const correctEmail = 'admin@gmail.com';
+//     const correctPassword = 'admin123';
  
-    when(mockAuthUsecase.loginUser(any, any)).thenAnswer((invocation) {
-      final email = invocation.positionalArguments[0] as String;
-      final password = invocation.positionalArguments[1] as String;
-      return Future.value(email == correctEmail && password == correctPassword
-          ? const Right(true)
-          : Left(Failure(error: 'Invalid')));
-    });
-    //Act
-    await container
-        .read(authViewModelProvider.notifier)
-        .loginUser("admin@gmail.com", "admin123");
+//     when(mockAuthUsecase.loginUser(any, any)).thenAnswer((invocation) {
+//       final email = invocation.positionalArguments[0] as String;
+//       final password = invocation.positionalArguments[1] as String;
+//       return Future.value(email == correctEmail && password == correctPassword
+//           ? const Right(true)
+//           : Left(Failure(error: 'Invalid')));
+//     });
+//     //Act
+//     await container
+//         .read(authViewModelProvider.notifier)
+//         .loginUser("admin@gmail.com", "admin123");
  
-    final authState = container.read(authViewModelProvider);
+//     final authState = container.read(authViewModelProvider);
  
-    //Assert
-    expect(authState.error, isNull);
-  });
-  test("Register user with valid credentials", () async {
-    //Arrange
-    when(mockAuthUsecase.registerUser(any)).thenAnswer((invocation) {
-      final student = invocation.positionalArguments[0] as AuthEntity;
-      return Future.value(
-        student.firstName.isNotEmpty &&
-                student.lastName.isNotEmpty &&
-                student.email.isNotEmpty &&
-                student.password.isNotEmpty &&
-                student.email.contains('@') &&
-                student.email.contains('.') &&
-                student.phone.length == 10
-            ? const Right(true)
-            : Left(
-                Failure(error: 'Invalid'),
-              ),
-      );
-    });
+//     //Assert
+//     expect(authState.error, isNull);
+//   });
+//   test("Register user with valid credentials", () async {
+//     //Arrange
+//     when(mockAuthUsecase.registerUser(any)).thenAnswer((invocation) {
+//       final student = invocation.positionalArguments[0] as AuthEntity;
+//       return Future.value(
+//         student.firstName.isNotEmpty &&
+//                 student.lastName.isNotEmpty &&
+//                 student.email.isNotEmpty &&
+//                 student.password.isNotEmpty &&
+//                 student.email.contains('@') &&
+//                 student.email.contains('.') &&
+//                 student.phone.length == 10
+//             ? const Right(true)
+//             : Left(
+//                 Failure(error: 'Invalid'),
+//               ),
+//       );
+//     });
  
-    //Act
-    await container
-        .read(authViewModelProvider.notifier)
-        .registerUser(const AuthEntity(
-          firstName: 'admin',
-          lastName: 'admin',
-          email: 'admin@gmail',
-          phone: '1234567890',
-          password: 'admin123',
-        ));
+//     //Act
+//     await container
+//         .read(authViewModelProvider.notifier)
+//         .registerUser(const AuthEntity(
+//           firstName: 'admin',
+//           lastName: 'admin',
+//           email: 'admin@gmail',
+//           phone: '1234567890',
+//           password: 'admin123',
+//         ));
  
-    final state = container.read(authViewModelProvider);
+//     final state = container.read(authViewModelProvider);
  
-    //Assert
-    expect(state.isLoading, true);
-    expect(state.error, null);
-  });
-  tearDown((){
-    container.dispose();
-  });
-}
+//     //Assert
+//     expect(state.isLoading, true);
+//     expect(state.error, null);
+//   });
+//   tearDown((){ 
+//     container.dispose();
+//   });
+// }
