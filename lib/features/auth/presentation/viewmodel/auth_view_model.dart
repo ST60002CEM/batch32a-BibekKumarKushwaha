7acc324953424806bd2c9c35187722eafc80cd85
BibekKumarKushwaha <bibekkumar345@gmail.com>
2024-08-08 16:41:22 +0545

    auth view model

--- lib/features/auth/presentation/viewmodel/auth_view_model.dart
+++ lib/features/auth/presentation/viewmodel/auth_view_model.dart
@@ -5,6 +5,7 @@ import 'package:final_assignment/features/auth/presentation/navigator/login_navi
 import 'package:final_assignment/features/auth/presentation/state/auth_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:local_auth/local_auth.dart';
 
 final authViewModelProvider = StateNotifierProvider<AuthViewModel, AuthState>(
   (ref) => AuthViewModel(
@@ -17,6 +18,7 @@ class AuthViewModel extends StateNotifier<AuthState> {
   AuthViewModel(this.navigator, this.authUseCase) : super(AuthState.initial());
   final AuthUseCase authUseCase;
   final LoginViewNavigator navigator;
+  late LocalAuthentication _localAuth;
 
   Future<void> registerUser(AuthEntity student) async {
     state = state.copyWith(isLoading: true);
@@ -42,16 +44,16 @@ class AuthViewModel extends StateNotifier<AuthState> {
   ) async {
     state = state.copyWith(isLoading: true);
     var data = await authUseCase.loginUser(email, password);
-    // data.fold(
-    //   (l) {
-    //     state = state.copyWith(isLoading: false, error: l.error);
-    //     showMySnackBar(message: l.error, color: Colors.red);
-    //   },
-    //   (r) {
-    //     state = state.copyWith(isLoading: false, error: null);
-    //     openHomeView();
-    //   },
-    // );
+    data.fold(
+      (l) {
+        state = state.copyWith(isLoading: false, error: l.error);
+        showMySnackBar(message: l.error, color: Colors.red);
+      },
+      (r) {
+        state = state.copyWith(isLoading: false, error: null);
+        openHomeView();
+      },
+    );
   }
 
   void openRegisterView() {
@@ -61,4 +63,43 @@ class AuthViewModel extends StateNotifier<AuthState> {
   void openHomeView() {
     navigator.openHomeView();
   }
+  Future<void> fingerPrintLogin() async {
+    _localAuth = LocalAuthentication();
+ 
+    bool authenticated = false;
+    try {
+      authenticated = await _localAuth.authenticate(
+        localizedReason: 'Authenticate to enable fingerprint',
+        options: const AuthenticationOptions(
+          stickyAuth: true,
+          biometricOnly: true,
+          useErrorDialogs: true,
+        ),
+      );
+    } catch (e) {
+      showMySnackBar(
+          message: 'Fingerprint authentication failed', color: Colors.red);
+    }
+ 
+    if (authenticated) {
+      authUseCase.fingerPrintLogin().then((data) {
+        data.fold(
+          (l) {
+            showMySnackBar(message: l.error, color: Colors.red);
+          },
+          (r) {
+            showMySnackBar(message: "User logged in successfully");
+            navigator.openHomeView();
+          },
+        );
+      });
+    } else {
+      showMySnackBar(
+          message: 'Fingerprint authentication failed', color: Colors.red);
+    }
+  }
+    void logout() async {
+    navigator.openLoginView();
+  }
+ 
 }
