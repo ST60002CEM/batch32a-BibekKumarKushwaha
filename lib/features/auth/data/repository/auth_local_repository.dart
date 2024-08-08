75b76cad722c2d4ac769b6aa093576e289126300
BibekKumarKushwaha <bibekkumar345@gmail.com>
2024-08-08 16:40:12 +0545

    auth local repo

--- lib/features/auth/data/repository/auth_local_repository.dart
+++ lib/features/auth/data/repository/auth_local_repository.dart
@@ -25,4 +25,22 @@ class AuthLocalRepository implements IAuthRepository {
   Future<Either<Failure, bool>> registerUser(AuthEntity user) {
     return _authLocalDataSource.registerStudent(user);
   }
+
+    @override
+  Future<Either<Failure, bool>> fingerPrintLogin(String id) {
+    // TODO: implement fingerPrintLogin
+    throw UnimplementedError();
+  }
+ 
+  @override
+  Future<Either<Failure, AuthEntity>> getCurrentUser() {
+    // TODO: implement getCurrentUser
+    throw UnimplementedError();
+  }
+ 
+  @override
+  Future<Either<Failure, bool>> verifyUser() {
+    // TODO: implement verifyUser
+    throw UnimplementedError();
+  }
 }
