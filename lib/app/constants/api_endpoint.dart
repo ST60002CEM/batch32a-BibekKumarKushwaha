6285366131e3394836d195262f082ef031045747
BibekKumarKushwaha <bibekkumar345@gmail.com>
2024-08-08 16:37:37 +0545

    endpoint

--- lib/app/constants/api_endpoint.dart
+++ lib/app/constants/api_endpoint.dart
@@ -3,7 +3,7 @@ class ApiEndpoints {
 
   static const Duration connectionTimeout = Duration(seconds: 1000);
   static const Duration receiveTimeout = Duration(seconds: 1000);
-  static const String baseUrl = "http://10.0.2.2:5000/api/";
+  static const String baseUrl = "http://172.25.0.114:5000/api/";
   //static const String baseUrl = "http://localhost:3000/api/v1/";
   //static const String baseUrl = "http://192.168.4.4:3000/api/v1/";
   //static const String products = '$baseUrl/admin/dashboard/products';
@@ -11,6 +11,9 @@ class ApiEndpoints {
   // ====================== Auth Routes ======================
   static const String login = "user/login";
   static const String register = "user/create";
+  static const String getMe = "user/profile/get";
+  static const String getToken ="user/getToken";
+  static const String verifyUser = "user/verifyUser";
   
   
 // ============Product Routes ==========
@@ -19,9 +22,10 @@ class ApiEndpoints {
 
   static const String paginatonProducts = 'product/pagination';
   static const int limitPage = 5;
+  
 
   // static const String imageUrl = 'http://192.168.1.133:5000/products/'
-  static const String imageUrl = 'http://10.0.2.2:5000/products/';
+  static const String imageUrl = 'http://172.25.0.114:5000/products/';
 }
 
 
