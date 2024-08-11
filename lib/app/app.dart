ab226c6b5c5ba94c0ba84775ed3fd16ae34c1c3d
BibekKumarKushwaha <bibekkumar345@gmail.com>
2024-08-11 22:39:30 +0545

    app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,25 +1,51 @@
 import 'package:final_assignment/app/navigator_key/navigator_key.dart';
 import 'package:final_assignment/app/themes/app_theme.dart';
-import 'package:final_assignment/app/themes/light_dark_theme.dart';
+
+import 'package:final_assignment/core/common/provider/theme_view_model_provider.dart';
 import 'package:final_assignment/features/splash/presentation/view/splash_view.dart';
-
 import 'package:flutter/material.dart';
-
-
-class App extends StatelessWidget {
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:light_sensor/light_sensor.dart';
+ 
+class App extends ConsumerStatefulWidget {
   const App({super.key});
-
+ 
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+ 
+class _MyAppState extends ConsumerState<App> {
+  @override
+  void initState() {
+    super.initState();
+    _initializeLightSensor();
+  }
+ 
+  void _initializeLightSensor() async {
+    final bool hasSensor = await LightSensor.hasSensor();
+    if (hasSensor) {
+      LightSensor.luxStream().listen((int lux) {
+        final isDarkMode = lux < 5000;
+        ref.read(themeViewModelProvider.notifier).state = isDarkMode;
+      });
+    }
+  }
+ 
   @override
   Widget build(BuildContext context) {
+    final isDarkMode = ref.watch(themeViewModelProvider);
+    
+    
+    
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // theme: AppTheme.getApplicationTheme(false),
-      themeMode: ThemeMode.system,
-      theme: KAppTheme.lightTheme,
-      darkTheme: KAppTheme.darkTheme,
-      title: 'Too Ease',
-      home: const SplashView(),
       navigatorKey: AppNavigator.navigatorKey,
+      theme: AppTheme.getApplicationTheme(isDarkMode),
+    
+      home: const SplashView(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
+ 
+ 
+ 
