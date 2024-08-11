5ceb3ce8163c965b30e5cf2b4db97c4c740d6ec7
BibekKumarKushwaha <bibekkumar345@gmail.com>
2024-08-12 00:22:12 +0545

    theme view model

---
+++ lib/core/common/provider/theme_view_model_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+ 
+final themeViewModelProvider =
+    StateNotifierProvider<ThemeViewModel, bool>((ref) => ThemeViewModel());
+ 
+class ThemeViewModel extends StateNotifier<bool> {
+  ThemeViewModel() : super(false);
+ 
+  void changeTheme() {
+    state = !state;
+  }
+}
+ 
+ 
