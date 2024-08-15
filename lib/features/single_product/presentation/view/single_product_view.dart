d909a21085a9f2e593cf3fc3e2f9fc3051c194de
BibekKumarKushwaha <bibekkumar345@gmail.com>
2024-08-16 01:38:12 +0545

    single product view

--- lib/features/single_product/presentation/view/single_product_view.dart
+++ lib/features/single_product/presentation/view/single_product_view.dart
@@ -1,65 +1,67 @@
-// import 'package:final_assignment/features/cart/presentation/viewmodel/cart_view_model.dart';
-// import 'package:final_assignment/features/single_product/presentation/view_model/single_product_view_model.dart';
-// import 'package:final_assignment/features/single_product/presentation/widgets/my_single_product_view.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:final_assignment/features/cart/presentation/view_model/cart_view_model.dart';
 
-// class SingleProductView extends ConsumerStatefulWidget {
-//   final String productId;
+import 'package:final_assignment/features/single_product/presentation/view_model/single_product_view_model.dart';
+import 'package:final_assignment/features/single_product/presentation/widgets/my_single_product_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-//   const SingleProductView({required this.productId, super.key});
+class SingleProductView extends ConsumerStatefulWidget {
+  final String productId;
 
-//   @override
-//   _SingleProductViewState createState() => _SingleProductViewState();
-// }
+  const SingleProductView({required this.productId, super.key});
 
-// class _SingleProductViewState extends ConsumerState<SingleProductView> {
-//   @override
-//   void initState() {
-//     super.initState();
-//     _fetchProductDetails();
-//   }
+  @override
+  _SingleProductViewState createState() => _SingleProductViewState();
+}
 
-//   void _fetchProductDetails() {
-//     Future.microtask(() {
-//       ref
-//           .read(singleProductViewModelProvider.notifier)
-//           .getSingleProduct(widget.productId);
-//     });
-//   }
+class _SingleProductViewState extends ConsumerState<SingleProductView> {
+  @override
+  void initState() {
+    super.initState();
+    _fetchProductDetails();
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     final productState = ref.watch(singleProductViewModelProvider);
+  void _fetchProductDetails() {
+    Future.microtask(() {
+      ref
+          .read(singleProductViewModelProvider.notifier)
+          .getSingleProduct(widget.productId);
+    });
+  }
 
-//     return Scaffold(
-//       body: productState.isLoading
-//           ? const Center(child: CircularProgressIndicator())
-//           : productState.error != null
-//               ? Center(child: Text(productState.error!))
-//               : productState.singleProduct != null
-//                   ? SingleView(
-//                       singleProduct: productState.singleProduct!,
-//                       selectedQuantity: productState.selectedQuantity,
-//                       onAddToBag: () {
-//                         ref.read(cartViewModelProvider.notifier).addCart(
-//                             productState.singleProduct!.id!,
-//                             productState.selectedQuantity);
-//                       },
-//                       onIncreaseQuantity: () {
-//                         ref
-//                             .read(singleProductViewModelProvider.notifier)
-//                             .updateSelectedQuantity(
-//                                 productState.selectedQuantity + 1);
-//                       },
-//                       onDecreaseQuantity: () {
-//                         ref
-//                             .read(singleProductViewModelProvider.notifier)
-//                             .updateSelectedQuantity(
-//                                 productState.selectedQuantity - 1);
-//                       },
-//                     )
-//                   : const Center(child: Text('Product not found')),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    final productState = ref.watch(singleProductViewModelProvider);
+
+    return Scaffold(
+      body: productState.isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : productState.error != null
+              ? Center(child: Text(productState.error!))
+              : productState.singleProduct != null
+                  ? SingleView(
+                      singleProduct: productState.singleProduct!,
+                      selectedQuantity: productState.selectedQuantity,
+                      onAddToBag: () {
+                        ref.read(cartViewModelProvider.notifier).addCart(
+                            productState.singleProduct!.id!,
+                            productState.selectedQuantity,
+                            productState.singleProduct!.productPrice);
+                      },
+                      onIncreaseQuantity: () {
+                        ref
+                            .read(singleProductViewModelProvider.notifier)
+                            .updateSelectedQuantity(
+                                productState.selectedQuantity + 1);
+                      },
+                      onDecreaseQuantity: () {
+                        ref
+                            .read(singleProductViewModelProvider.notifier)
+                            .updateSelectedQuantity(
+                                productState.selectedQuantity - 1);
+                      },
+                    )
+                  : const Center(child: Text('Product not found')),
+    );
+  }
+}
