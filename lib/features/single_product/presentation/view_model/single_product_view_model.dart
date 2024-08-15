755d02d5991ec25efcaa352a35fece07a8dfc642
BibekKumarKushwaha <bibekkumar345@gmail.com>
2024-08-16 01:38:26 +0545

    single product view

--- lib/features/single_product/presentation/view_model/single_product_view_model.dart
+++ lib/features/single_product/presentation/view_model/single_product_view_model.dart
@@ -1,52 +1,51 @@
-// import 'package:final_assignment/core/common/widgets/my_snackbar.dart';
-// import 'package:final_assignment/core/shared_prefs/user_shared_prefs.dart';
-// import 'package:final_assignment/features/home/domain/usecases/product_usecase.dart';
-// import 'package:final_assignment/features/product/domain/usecase/product_usecase.dart';
-// import 'package:final_assignment/features/single_product/presentation/state/single_product_state.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:final_assignment/core/common/widgets/my_snackbar.dart';
+import 'package:final_assignment/core/shared_prefs/user_shared_prefs.dart';
+import 'package:final_assignment/features/home/domain/usecases/product_usecase.dart';
+import 'package:final_assignment/features/single_product/presentation/state/single_product_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// final singleProductViewModelProvider =
-//     StateNotifierProvider<SingleProductViewModel, SingleProductState>(
-//         (ref) => SingleProductViewModel(
-//               productUsecase: ref.watch(productUsecaseProvider),
-//               userSharedPrefs: ref.watch(userSharedPrefsProvider),
-//             ));
+final singleProductViewModelProvider =
+    StateNotifierProvider<SingleProductViewModel, SingleProductState>(
+        (ref) => SingleProductViewModel(
+              productUsecase: ref.watch(productUsecaseProvider),
+              userSharedPrefs: ref.watch(userSharedPrefsProvider),
+            ));
 
-// class SingleProductViewModel extends StateNotifier<SingleProductState> {
-//   SingleProductViewModel({
-//     required this.productUsecase,
-//     required this.userSharedPrefs,
-//   }) : super(SingleProductState.initial());
+class SingleProductViewModel extends StateNotifier<SingleProductState> {
+  SingleProductViewModel({
+    required this.productUsecase,
+    required this.userSharedPrefs,
+  }) : super(SingleProductState.initial());
 
-//   final ProductUsecase productUsecase;
-//   final UserSharedPrefs userSharedPrefs;
+  final ProductUsecase productUsecase;
+  final UserSharedPrefs userSharedPrefs;
 
-//   Future<void> getSingleProduct(String productId) async {
-//     state = state.copyWith(isLoading: true);
-//     final result = await productUsecase.getProductById(productId);
-//     result.fold((failure) {
-//       state = state.copyWith(isLoading: false, error: failure.error);
-//       showMySnackBar(message: failure.error, color: Colors.red);
-//     }, (data) {
-//       state = state.copyWith(
-//         isLoading: false,
-//         singleProduct: data,
-//         error: null,
-//       );
-//     });
-//   }
+  Future<void> getSingleProduct(String productId) async {
+    state = state.copyWith(isLoading: true);
+    final result = await productUsecase.getProductById(productId);
+    result.fold((failure) {
+      state = state.copyWith(isLoading: false, error: failure.error);
+      showMySnackBar(message: failure.error, backgroundColor: Colors.red);
+    }, (data) {
+      state = state.copyWith(
+        isLoading: false,
+        singleProduct: data,
+        error: null,
+      );
+    });
+  }
 
-//   void updateSelectedQuantity(int quantity) {
-//     if (state.singleProduct != null &&
-//         quantity > state.singleProduct!.productQuantity) {
-//       showMySnackBar(message: 'Out of stock', color: Colors.red);
-//       return;
-//     }
-//     if (quantity < 1) {
-//       showMySnackBar(message: 'Quantity cannot be less 1', color: Colors.red);
-//       return;
-//     }
-//     state = state.copyWith(selectedQuantity: quantity);
-//   }
-// }
+  void updateSelectedQuantity(int quantity) {
+    if (state.singleProduct != null &&
+        quantity > state.singleProduct!.productPrice) {
+      showMySnackBar(message: 'Out of stock', backgroundColor: Colors.red);
+      return;
+    }
+    if (quantity < 1) {
+      showMySnackBar(message: 'Quantity cannot be less 1', backgroundColor: Colors.red);
+      return;
+    }
+    state = state.copyWith(selectedQuantity: quantity);
+  }
+}
