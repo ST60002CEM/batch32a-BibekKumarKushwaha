97698855955966a9c76f519964c72fc61b942a29
BibekKumarKushwaha <bibekkumar345@gmail.com>
2024-08-15 22:21:41 +0545

    khalti navigator

---
+++ lib/features/khalti/presentation/navigator/khalti_navigator.dart
@@ -0,0 +1,20 @@
+// import 'package:final_assignment/app/navigator/navigator.dart';
+// import 'package:final_assignment/features/booking/domain/entity/booking_entity.dart';
+// import 'package:final_assignment/features/khalti/presentation/view/khalti_pay_view.dart';
+// import 'package:final_assignment/features/payment_success/presentation/navigator/payment_success_navigator.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// import '../../../booking/domain/entity/booking_entity.dart';
+// import '../../../payment_success/presentation/navigator/payment_success_navigator.dart';
+
+// final khaltiViewNavigatorProvider = Provider<KhaltiViewNavigator>((ref) {
+//   return KhaltiViewNavigator();
+// });
+
+// class KhaltiViewNavigator with PaymentSuccessViewRoute {}
+
+// mixin KhaltiViewRoute {
+//   void openKhaltiView(String pidx,BookingEntity bookingEntity) {
+//     NavigateRoute.pushRoute(KhaltiSDKDemo(pidx: pidx,entity: bookingEntity));
+//   }
+// }
