8395809d5d684a72bcaba7d95f4af8eb65bb1569
BibekKumarKushwaha <bibekkumar345@gmail.com>
2024-08-15 22:22:54 +0545

    single product navigator

---
+++ lib/features/single_product/presentation/navigator/single_product_navigator.dart
@@ -0,0 +1,11 @@
+// import 'package:final_assignment/app/navigator/navigator.dart';
+// import 'package:final_assignment/features/single_product/presentation/view/single_product_view.dart';
+
+// class SingleProductViewNavigator{}
+
+// mixin SingleProductViewRoute{
+//   void navigateToSingleProductView(){
+//     NavigateRoute.pushRoute(const SingleProductView());
+//   }
+
+// }
